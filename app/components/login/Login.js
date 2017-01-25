d954bb477d0ed85ddf275d770cbcd0364bf989f2
Toto RoToto <el.totorototo@gmail.com>
2017-01-25 23:36:47 +0100

    refacto - login - webview

--- app/components/login/Login.js
+++ app/components/login/Login.js
@@ -1,42 +1,34 @@
 'use strict';
 import React, {Component, PropTypes} from 'react';
 
-import {View, Text, Linking} from 'react-native';
+import {View, Text, Linking, WebView} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
 export default class Login extends Component {
 
-    componentDidMount() {
-
-        let stravaUrl = 'https://www.strava.com/oauth/authorize?client_id=15688&state=mystate&response_type=code&redirect_uri=strava://localhost&scope=public&approval_prompt=auto';
-
-        Linking.addEventListener('url', this.handleOpenURL);
-
-        Linking.canOpenURL(stravaUrl).then(supported => {
-            if (supported) {
-                Linking.openURL(stravaUrl);
-            } else {
-                console.log('Don\'t know how to open URI: ' + this.props.url);
+    handleNavigationChange(event){
+        if(this.refs != null){
+            if (event.url.indexOf('strava://localhost') === -1) {
+                return true;
+            }else{
+                this.refs['toto'].stopLoading(); //Some reference to your WebView to make it stop loading that URL
+                return false;
             }
-        });
-    }
-
-    componentWillUnmount() {
-        Linking.removeEventListener('url', this.handleOpenURL);
-    }
-
-    handleOpenURL(event) {
-        console.log(event.url);
+        }
+        return true;
     }
 
     render() {
 
         const {login} = this.props;
 
+        const stravaUrl = 'https://www.strava.com/oauth/authorize?client_id=15688&response_type=code&redirect_uri=strava://localhost&scope=public';
+
         return (
-            <View style={{margin: 128}}>
-                <Text onPress={login}>This is login!</Text>
-            </View>
+            <WebView
+                source={{uri: stravaUrl}}
+                style={{marginTop: 20}}
+            />
         );
     }
 }
--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import{Linking} from 'react-native';
 
 import configureStore from './store/configureStore';
 
@@ -7,18 +8,30 @@ import { Actions, Router, Scene } from 'react-native-router-flux'
 import HomeContainer from './routes/home/containers/HomeContainer'
 import LoginContainer from './routes/login/containers/LoginContainer'
 
+
 import { Provider, connect } from 'react-redux'
 
 //TODO: refacto -> move to /route/index.js
 const Scenes = Actions.create(
     <Scene key="root" hideNavBar={true}>
         <Scene key="login" component={LoginContainer} hideNavBar={true}/>
-        <Scene key="home" component={HomeContainer}/>
+        <Scene key="localhost" component={HomeContainer}/>
     </Scene>
 );
 
 //TODO: AppContainer?
 const ConnectedRouter = connect()(Router);
+
+function handleOpenURL(url) {
+
+    if(url != null){
+        let arr = url.split(/&code=/);
+        let token = arr[1];
+        Actions.localhost();
+    }
+}
+
+Linking.addEventListener('url',(event) => handleOpenURL(event.url));
 
 export default class App extends Component {
 
@@ -27,6 +40,17 @@ export default class App extends Component {
         this.state = {
             store: configureStore(),
         };
+    }
+
+    componentDidMount() {
+
+        let url = Linking.getInitialURL()
+            .then(handleOpenURL)
+            .catch(err => console.error('An error occurred', err));
+    }
+
+    componentWillUnmount() {
+        Linking.removeEventListener('url', handleOpenURL);
     }
 
     render() {
