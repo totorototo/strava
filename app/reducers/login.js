72c2858f7a7046e1b4c0b5c6cb4c01bf70d48faa
Toto RoToto <el.totorototo@gmail.com>
2017-02-05 11:27:46 +0100

    fix import

--- app/reducers/login.js
+++ app/reducers/login.js
@@ -1,14 +1,14 @@
 //constants
-import * as types from './../constants/actionTypes'
+import {GET_ACCESS_TOKEN} from './../constants/actionTypes'
 
 const initialState = {
     access_token: ''
-}
+};
 
 export default function reducer(state = initialState, action) {
 
     switch (action.type) {
-        case types.GET_ACCESS_TOKEN:
+        case GET_ACCESS_TOKEN:
 
             return Object.assign({}, state, {
                 access_token: action.access_token
