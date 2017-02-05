63ba5e5d46ffe65a5738c4c541945531c83355f1
Toto RoToto <el.totorototo@gmail.com>
2017-02-05 11:26:42 +0100

    fix imports and update actions

--- app/actions/login.js
+++ app/actions/login.js
@@ -1,27 +1,27 @@
-import * as types from "../constants/actionTypes";
+import {LOGIN, LOGOUT, GET_TEMPORARY_ACCESS_TOKEN, GET_ACCESS_TOKEN, } from "../constants/actionTypes";
 
 export function login() {
     return {
-        type: types.LOGIN
+        type: LOGIN
     }
 }
 
 export function getTemporaryAcessToken(temporary_access_token){
     return{
-        type: types.GET_TEMPORARY_ACCESS_TOKEN,
+        type: GET_TEMPORARY_ACCESS_TOKEN,
         temporary_access_token
     }
 }
 
 export function getAccessToken(access_token){
     return{
-        type: types.GET_ACCESS_TOKEN,
+        type: GET_ACCESS_TOKEN,
         access_token
     }
 }
 
 export function logout(){
     return{
-        type: types.LOGOUT
+        type: LOGOUT
     }
 }
