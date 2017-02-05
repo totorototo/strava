6dad9ff0bd49f28ed8404120ba99f48309095d6b
Toto RoToto <el.totorototo@gmail.com>
2017-02-05 11:23:04 +0100

    fix import

--- app/reducers/athlete.js
+++ app/reducers/athlete.js
@@ -1,14 +1,14 @@
 //constants
-import * as types from './../constants/actionTypes'
+import {GET_ATHLETE_DETAILS} from './../constants/actionTypes'
 
 const initialState = {
     athlete: {}
-}
+};
 
 export default function reducer(state = initialState, action) {
 
     switch (action.type) {
-        case types.GET_ATHLETE_DETAILS:
+        case GET_ATHLETE_DETAILS:
 
             return Object.assign({}, state, {
                 athlete: action.details
