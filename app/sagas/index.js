7f0f4fc18943622dedaa15fdb63a8cb2ca8f96dc
Toto RoToto <el.totorototo@gmail.com>
2017-02-05 11:25:30 +0100

    remove useless import and fix import

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,14 +1,11 @@
 //redux-saga
-import { fork } from 'redux-saga/effects'
+import {fork} from 'redux-saga/effects'
 
 //sagas
-import * as authentication from './login'
-
-//action
-import * as types from '../constants/actionTypes'
+import {authenticationFlowSaga} from './login'
 
 export default function* root() {
     yield [
-        fork (authentication.authenticationFlowSaga)
+        fork (authenticationFlowSaga)
     ]
 }
