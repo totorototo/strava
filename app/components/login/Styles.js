5b280d0f7485219f8135891a84ab0a48bbde2e57
Toto RoToto <el.totorototo@gmail.com>
2017-02-13 21:38:08 +0100

    split styles.

---
+++ app/components/login/Styles.js
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  login: {
+    marginTop: 20,
+  },
+});
+
+module.exports = styles;
