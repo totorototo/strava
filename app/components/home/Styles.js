caacfff315fe624d9646a5bd04e40dbb522c12c7
Toto RoToto <el.totorototo@gmail.com>
2017-02-13 21:47:05 +0100

    split styles.

---
+++ app/components/home/Styles.js
@@ -0,0 +1,10 @@
+import React, { Component, PropTypes } from 'react';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  home: {
+    margin: 128,
+  },
+});
+
+module.exports = styles;
