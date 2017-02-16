e8b00af01e686b7fe5b942cd7277381d8556e01f
totorototo <el.totorototo@gmail.com>
2017-02-16 18:29:40 +0100

    TEMP: hack for navigation reducer issue.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -2,10 +2,13 @@ import { combineReducers } from 'redux';
 
 import login from './login';
 import athlete from './athlete';
-import navigation from './navigation';
+import { AppNavigator } from '../routes/index';
 
 export default combineReducers({
+  // TODO:
+  nav: (state, action) => (
+     AppNavigator.router.getStateForAction(action, state)
+  ),
   login,
   athlete,
-  navigation,
 });
