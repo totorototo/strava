fe6b6e57de54d370193cd40feb1f6803ba4eadbb
totorototo <el.totorototo@gmail.com>
2017-02-16 17:56:47 +0100

    add navigation reducer.

---
+++ app/reducers/navigation.js
@@ -0,0 +1,11 @@
+import { AppNavigator } from '../routes/index';
+
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+  return {
+    nav: (state, action) => (
+          AppNavigator.router.getStateForAction(action, state)
+      ),
+  };
+}
