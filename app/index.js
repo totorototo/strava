0e6f3da52546146ee9ffe27d58d98bcd7f0b3086
totorototo <el.totorototo@gmail.com>
2017-02-17 16:09:38 +0100

    deep linking issues still not resolved. Switch back to previous implementation.

--- app/index.js
+++ app/index.js
@@ -1,8 +1,12 @@
 // react-native
 import React, { Component } from 'react';
+import { Linking } from 'react-native';
 
 // redux
 import { Provider } from 'react-redux';
+
+// actions
+import { getTemporaryAcessToken } from './actions/login';
 
 // Container
 import AppWithNavigationState from './containers/AppContainer';
@@ -12,7 +16,30 @@ import configureStore from './store/configureStore';
 
 const store = configureStore();
 
+function handleOpenURL(url) {
+  if (url != null) {
+    const myRegexp = /(?:&code=)(\w*)/g;
+    const match = myRegexp.exec(url);
+    store.dispatch(getTemporaryAcessToken(match[1]));
+  }
+}
+
+
+Linking.addEventListener('url', event => handleOpenURL(event.url));
+
+
 export default class App extends Component {
+
+  componentDidMount() {
+      // eslint-disable no-console
+    Linking.getInitialURL()
+          .then(handleOpenURL)
+          .catch(err => console.error('An error occurred', err));
+  }
+
+  componentWillUnmount() {
+    Linking.removeEventListener('url', handleOpenURL);
+  }
 
   render() {
     return (
