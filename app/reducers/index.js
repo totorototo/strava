437ab3f55530573ced78ef202294ff65907152cc
Toto RoToto <el.totorototo@gmail.com>
2017-02-17 19:39:55 +0100

    add comment for hack.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -5,7 +5,7 @@ import athlete from './athlete';
 import { AppNavigator } from '../routes/index';
 
 export default combineReducers({
-  // TODO:
+  // TODO: hack. should import ans use navigation reducer instead.
   nav: (state, action) => (
      AppNavigator.router.getStateForAction(action, state)
   ),
