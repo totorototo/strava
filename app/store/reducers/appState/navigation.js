3e03e2c6fa7efae1adb28f96431ca610439858f3
Toto RoToto <el.totorototo@gmail.com>
2017-02-21 20:46:06 +0100

    code clean up. no need to block route.

--- app/store/reducers/appState/navigation.js
+++ app/store/reducers/appState/navigation.js
@@ -1,8 +1,5 @@
 import AppNavigator from '../../../routes/AppNavigator';
 
 export default function reducer(state, action) {
-  if(action.routeName === 'Home' && !state.isAuthenticated) {
-    return AppNavigator.router.getStateForAction({}, state);
-  }
   return AppNavigator.router.getStateForAction(action, state);
 }
