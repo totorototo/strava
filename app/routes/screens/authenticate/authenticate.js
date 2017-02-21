56e978542039276946376db17d5b87bfe42b42bc
Toto RoToto <el.totorototo@gmail.com>
2017-02-21 20:46:45 +0100

    Add authenticate component.

---
+++ app/routes/screens/authenticate/authenticate.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { View, Text } from 'react-native';
+
+const Authenticate = () => (
+  <View>
+    <Text>
+      Authenticate
+    </Text>
+  </View>
+);
+
+Authenticate.navigationOptions = {
+  title: 'Authenticate',
+};
+
+export default Authenticate;
