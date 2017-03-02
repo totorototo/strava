b1206ffb21451ff0eb8597e18e43c326508673f2
Toto RoToto <el.totorototo@gmail.com>
2017-03-02 21:58:49 +0100

    update entities reducer.

--- app/store/reducers/entities/entities.js
+++ app/store/reducers/entities/entities.js
@@ -1,7 +1,13 @@
 import { combineReducers } from 'redux';
 
 import athlete from './athlete';
+import activities from './activities';
+import clubs from './clubs';
+import gear from './gear';
 
 export default combineReducers({
   athlete,
+  activities,
+  clubs,
+  gear,
 });
