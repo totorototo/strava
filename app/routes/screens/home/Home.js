0aec7e586a1a9a6428346e908599d90aee1b39d4
Toto RoToto <el.totorototo@gmail.com>
2017-03-02 21:05:50 +0100

    display user details.

--- app/routes/screens/home/Home.js
+++ app/routes/screens/home/Home.js
@@ -1,19 +1,34 @@
 // react
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 // react-native
 import { View, Text } from 'react-native';
 
+// redux
+import { connect } from 'react-redux';
+
 import styles from './styles';
 
 // styles
-export default class Home extends Component {
+class Home extends Component {
+  static propTypes = {
+    athlete: PropTypes.shape({
+      firstname: PropTypes.string,
+      lastname: PropTypes.lastname,
+    }).isRequired,
+  };
 
   render() {
+    const { athlete } = this.props;
     return (
       <View style={styles.home}>
-        <Text>This is home!</Text>
+        <Text>{ athlete.details.firstname }</Text>
+        <Text>{ athlete.details.lastname }</Text>
       </View>
     );
   }
 }
+
+const mapStateToProps = state => ({ athlete: state.entities.athlete });
+
+export default connect(mapStateToProps)(Home);
