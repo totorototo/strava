b65260f7d757e822035ca3993ca7ee48f81b5cf3
totorototo <el.totorototo@gmail.com>
2017-03-03 17:30:00 +0100

    update athlete service.

--- app/store/actions/athlete.js
+++ app/store/actions/athlete.js
@@ -1,8 +1,55 @@
-import { RETRIEVE_ATHLETE_DETAILS } from '../constants/actionTypes';
+import {
+    RETRIEVE_ATHLETE_DETAILS,
+    GET_ATHLETE_ACTIVITIES,
+    RETRIEVE_ATHLETE_ACTIVITIES,
+    GET_ATHLETE_CLUBS,
+    RETRIEVE_ATHLETE_CLUBS,
+    GET_ATHLETE_GEAR,
+    RETRIEVE_ATHLETE_GEAR,
+} from '../constants/actionTypes';
 
 export function retrieveAthleteDetails(details) {
   return {
     type: RETRIEVE_ATHLETE_DETAILS,
     details,
+  };
+}
+
+export function getAthleteActivities() {
+  return {
+    type: GET_ATHLETE_ACTIVITIES,
+  };
+}
+
+export function retrieveAthleteActivities(activities) {
+  return {
+    type: RETRIEVE_ATHLETE_ACTIVITIES,
+    activities,
+  };
+}
+
+export function getAthleteClubs() {
+  return {
+    type: GET_ATHLETE_CLUBS,
+  };
+}
+
+export function retrieveAthleteClubs(clubs) {
+  return {
+    type: RETRIEVE_ATHLETE_CLUBS,
+    clubs,
+  };
+}
+
+export function getAthleteGear() {
+  return {
+    type: GET_ATHLETE_GEAR,
+  };
+}
+
+export function retrieveAthleteGear(gear) {
+  return {
+    type: RETRIEVE_ATHLETE_GEAR,
+    gear,
   };
 }
