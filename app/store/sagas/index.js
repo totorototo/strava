07602da5ee071b5ed1030ce4663cbeb8d0df8c76
totorototo <el.totorototo@gmail.com>
2017-03-03 17:32:49 +0100

    wip - update root saga.

--- app/store/sagas/index.js
+++ app/store/sagas/index.js
@@ -1,7 +1,16 @@
+// redux saga
+// import { takeEvery } from 'redux-saga';
+// import { fork } from 'redux-saga/effects';
+
 // sagas
 import { authenticationFlowSaga } from './login';
+// import { getClubs } from './athlete';
+
+// actions
+// import { GET_ATHLETE_CLUBS } from '../constants/actionTypes';
 
 export default [
   authenticationFlowSaga,
+  // takeEvery(GET_ATHLETE_CLUBS, getClubs),
 ];
 
