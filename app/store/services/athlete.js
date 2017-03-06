3adfeb5e1601bffd3f3f16e31aabed77bc481310
Toto RoToto <el.totorototo@gmail.com>
2017-03-06 21:20:01 +0100

    Update athlete service.

--- app/store/services/athlete.js
+++ app/store/services/athlete.js
@@ -1,23 +1,16 @@
 // constants
-import { API_ENDPOINT, APPLICATION_TYPE, RESOURCES, METHODS } from '../constants/rest';
+import { API_ENDPOINT, RESOURCES, METHODS } from '../constants/rest';
 
 // helper
 import { callJSONApi } from './helpers/api';
 
 export const getAthleteClubs = (token) => {
-  const headers = new Headers();
-  headers.append('Accept', 'application/json');
-  headers.append('Origin', '*');
-  headers.append('Cache-Control', 'no-cache');
-  headers.append('Content-Type', APPLICATION_TYPE.JSON);
-  headers.append('Authorization', `Bearer ${token}`);
-
   const request = {
     endpoint: {
       url: API_ENDPOINT + RESOURCES.ATHLETE_CLUB,
       httpVerb: METHODS.GET,
     },
-    headers,
+    token,
   };
   return callJSONApi(request);
 };
