2200484ad3043e87337b788f637d850a65539ac6
totorototo <el.totorototo@gmail.com>
2017-03-07 14:28:43 +0100

    update root saga.

--- app/store/sagas/index.js
+++ app/store/sagas/index.js
@@ -1,9 +1,11 @@
 // sagas
 import { authenticationFlowSaga } from './login';
 import { athleteSaga } from './athlete';
+import { activitySaga } from './activities';
 
 export default [
   authenticationFlowSaga,
   athleteSaga,
+  activitySaga,
 ];
 
