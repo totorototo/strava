fdc9addb52ab02bd7ea38c55f3e2fd19035aac10
totorototo <el.totorototo@gmail.com>
2017-03-07 14:25:18 +0100

    add activity reducer.

---
+++ app/store/reducers/entities/activities.js
@@ -0,0 +1,18 @@
+// constants
+import { RETRIEVE_ATHLETE_ACTIVITIES } from '../../constants/actionTypes';
+
+const initialState = {};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+
+    case RETRIEVE_ATHLETE_ACTIVITIES:
+
+      return Object.assign({}, state, action.activities);
+
+      // ...other actions
+
+    default:
+      return state;
+  }
+}
