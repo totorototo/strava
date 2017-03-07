e08c955d1fd1f870046a47edd36152964817e316
totorototo <el.totorototo@gmail.com>
2017-03-07 14:25:47 +0100

    add activity service.

---
+++ app/store/services/activities.js
@@ -0,0 +1,20 @@
+// constants
+import { API_ENDPOINT, RESOURCES, METHODS } from '../constants/rest';
+
+// helper
+import { callJSONApi } from './helpers/api';
+
+export const getAthleteActivities = (token) => {
+  const request = {
+    endpoint: {
+      url: API_ENDPOINT + RESOURCES.ACTIVITIES,
+      httpVerb: METHODS.GET,
+    },
+    token,
+  };
+  return callJSONApi(request)
+        .then(
+            response => (response.data),
+            error => (error),
+        );
+};
