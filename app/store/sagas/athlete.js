f7dc129bdd93ca74fef33453bb57c16220b65122
totorototo <el.totorototo@gmail.com>
2017-03-07 19:25:24 +0100

    add athlete stats saga.

--- app/store/sagas/athlete.js
+++ app/store/sagas/athlete.js
@@ -3,16 +3,17 @@ import { takeEvery } from 'redux-saga';
 import { put, call, select } from 'redux-saga/effects';
 
 // service
-import { getAthleteClubs } from '../services/athlete';
+import { getAthleteClubs, getAthleteStats } from '../services/athlete';
 
 // selector
 import getToken from '../selectors/token';
+import getAthleteID from '../selectors/athleteID';
 
 // actions
-import { GET_ATHLETE_CLUBS } from '../constants/actionTypes';
+import { GET_ATHLETE_CLUBS, GET_ATHLETE_STATS } from '../constants/actionTypes';
 
 // actions
-import { retrieveAthleteClubs } from '../actions/athlete';
+import { retrieveAthleteClubs, retrieveAthleteStats } from '../actions/athlete';
 
 function* getClubs() {
   try {
@@ -24,9 +25,21 @@ function* getClubs() {
   }
 }
 
+function* getStats() {
+  try {
+    const token = yield select(getToken);
+    const id = yield select(getAthleteID);
+    const stats = yield call(getAthleteStats, token, id);
+    yield put(retrieveAthleteStats(stats));
+  } catch (error) {
+    throw error;
+  }
+}
+
 
 export function* athleteSaga() {
   yield [
     takeEvery(GET_ATHLETE_CLUBS, getClubs),
+    takeEvery(GET_ATHLETE_STATS, getStats),
   ];
 }
