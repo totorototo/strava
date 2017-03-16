aea2d6316fec78a67ba61b0aafa26280aa56d800
totorototo <el.totorototo@gmail.com>
2017-03-16 16:42:06 +0100

    update root reducer.

--- app/store/reducers/entities/entities.js
+++ app/store/reducers/entities/entities.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import athlete from './athlete';
 import activities from './activities';
+import entity from './entity';
 
 export default combineReducers({
   athlete,
   activities,
+  entity,
 });
