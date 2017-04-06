dc8046358f95111968402c3948b0aff778953ed5
toto rototo <el.totorototo@gmail.com>
2017-04-06 17:26:06 +0200

    clean-up

--- app/store/reducers/entities/entities.js
+++ app/store/reducers/entities/entities.js
@@ -2,10 +2,8 @@ import { combineReducers } from 'redux';
 
 import athletes from './athletes';
 import activities from './activities';
-import entity from './entity';
 
 export default combineReducers({
   athletes,
   activities,
-  entity,
 });
