3c7af088e5fb8b529f0af8eb105753c45f25b8f2
toto rototo <el.totorototo@gmail.com>
2017-05-03 14:11:22 +0200

    add app state reducer.

---
+++ app/store/reducers/appState/data.js
@@ -0,0 +1,20 @@
+import { SET_CURRENT_USER_ID } from "../../constants/actionTypes";
+
+const initialState = {
+  currentUserID: ""
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case SET_CURRENT_USER_ID:
+      return {
+        ...state,
+        currentUserID: action.currentUserID
+      };
+
+    // ...other actions
+
+    default:
+      return state;
+  }
+}
