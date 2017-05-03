929393590792f92202f78a42a236739c186085d9
toto rototo <el.totorototo@gmail.com>
2017-05-03 11:45:12 +0200

    update root reducer.

--- app/store/reducers/appState/appState.js
+++ app/store/reducers/appState/appState.js
@@ -3,7 +3,21 @@ import { combineReducers } from "redux";
 import login from "./login";
 import navigation from "./navigation";
 
+import { SET_CURRENT_USER_ID } from "../../constants/actionTypes";
+
 export default combineReducers({
+  "@@/data": (state = {}, action) => {
+    switch (action.type) {
+      case SET_CURRENT_USER_ID:
+        return {
+          ...state,
+          currentUserID: action.currentUserID
+        };
+      // ...other actions
+      default:
+        return state;
+    }
+  },
   navigation,
   login
 });
