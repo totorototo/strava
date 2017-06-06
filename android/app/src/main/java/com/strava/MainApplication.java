5822f1e3d43b8b58f9636314a39e6d797ec30654
Toto RoToto <el.totorototo@gmail.com>
2017-06-06 20:45:01 +0200

    resolve conflict.

--- android/app/src/main/java/com/strava/MainApplication.java
+++ android/app/src/main/java/com/strava/MainApplication.java
@@ -3,11 +3,8 @@ package com.strava;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-<<<<<<< ours
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
-=======
->>>>>>> theirs
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
