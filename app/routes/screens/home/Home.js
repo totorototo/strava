480db36668f79843c457c8144e08a61fae442278
Toto RoToto <el.totorototo@gmail.com>
2017-06-27 12:01:36 +0200

    add fonts (icons) for ios and android. add dependencies. update tabnavigator.

--- app/routes/screens/home/Home.js
+++ app/routes/screens/home/Home.js
@@ -1,4 +1,8 @@
+// react
+import React from "react";
+
 import { TabNavigator } from "react-navigation";
+import { Icon } from "react-native-elements";
 
 import Details from "../details/Details";
 import ClubFeed from "../clubFeed/ClubFeed";
@@ -10,34 +14,39 @@ const Home = TabNavigator(
     PerformanceMeter: {
       screen: PerformanceMeter,
       navigationOptions: {
-        tabBarLabel: "Lybitometer"
+        tabBarLabel: "Lybito-mtr",
+        tabBarIcon: () => <Icon name="whatshot" color="#FC4C02" />
       }
     },
     RacePredictor: {
       screen: RacePredictor,
       navigationOptions: {
-        tabBarLabel: "predictor"
+        tabBarLabel: "predictor",
+        tabBarIcon: () => <Icon name="timer" color="#FC4C02" />
       }
     },
     Details: {
       screen: Details,
       navigationOptions: {
-        tabBarLabel: "athlete"
+        tabBarLabel: "athlete",
+        tabBarIcon: () => <Icon name="face" color="#FC4C02" />
       }
     },
     ClubFeed: {
       screen: ClubFeed,
       navigationOptions: {
-        tabBarLabel: "leaks"
+        tabBarLabel: "leaks",
+        tabBarIcon: () => <Icon name="comment" color="#FC4C02" />
       }
     }
   },
   {
     tabBarPosition: "bottom",
     tabBarOptions: {
+      showIcon: "true",
       labelStyle: {
         fontSize: 12,
-        color: "grey"
+        color: "#FC4C02"
       },
       style: {
         backgroundColor: "white"
