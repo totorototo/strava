6e3fb2b0fbaf476416394981e90257c77f06c38f
toto rototo <el.totorototo@gmail.com>
2017-06-28 20:03:08 +0200

    add athlete saga

---
+++ app/store/sagas/athlete.js
@@ -0,0 +1,25 @@
+// redux saga
+import { takeEvery } from "redux-saga";
+import { call, select } from "redux-saga/effects";
+
+// actions
+import { GET_ATHLETE_STATS } from "../constants/actionTypes";
+
+// selectors
+import { token, currentUserID } from "../selectors/app";
+
+// services
+import { getAthleteStats } from "../services/athlete";
+
+function* getStats() {
+  const accessToken = yield select(token);
+  const id = yield select(currentUserID);
+  const { error } = yield call(getAthleteStats, accessToken, id);
+  if (error) {
+    console.log(error);
+  }
+}
+
+export function* athleteSaga() {
+  yield takeEvery(GET_ATHLETE_STATS, getStats);
+}
