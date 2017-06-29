a6503a346c7a86fbf9ece103d0dc1c7f9191ad45
toto rototo <el.totorototo@gmail.com>
2017-06-29 19:44:37 +0200

    add entities action

--- app/store/actions/entities.js
+++ app/store/actions/entities.js
@@ -1,8 +1,17 @@
-import { SET_ENTITIES } from "../constants/actionTypes";
+import { SET_ENTITIES, UPDATE_ENTITIES } from "../constants/actionTypes";
 
 export function setEntities(entities) {
   return {
     type: SET_ENTITIES,
     entities
+  };
+}
+
+export function updateEntities(id, entityType, payload) {
+  return {
+    type: UPDATE_ENTITIES,
+    id,
+    entityType,
+    payload
   };
 }
