d9d34ee4f58affbcf829d990d7bc8b3001e6828a
guipasmoi <guipasmoi@hotmail.com>
2017-07-02 15:57:29 +0200

    fix: flow starting point

--- app/store/state/entities/selectors.js
+++ app/store/state/entities/selectors.js
@@ -1,14 +1,21 @@
+/* @flow */
 import { get } from "lodash";
 
 import { NotFoundEntity, isValid } from "../../../dataDefinitions/defects";
 
-export const getEntity = (state, entityType, id) =>
+export const getEntity = (state: StoreState, entityType: string, id: Id) =>
   get(state, `entities.${entityType}.${id}`) === undefined
     ? NotFoundEntity
     : state.entities[entityType][id];
 
-export const getEntities = (state, entityType, idList) =>
-  idList.map(id => getEntities(state, entityType, id));
+export const getEntities = (
+  state: StoreState,
+  entityType: string,
+  idList: Array<Id>
+) => idList.map(id => getEntity(state, entityType, id));
 
-export const getValidEntities = (state, entityType, idList) =>
-  getEntities(state, entityType, idList).filter(isValid);
+export const getValidEntities = (
+  state: StoreState,
+  entityType: string,
+  idList: Array<Id>
+) => getEntities(state, entityType, idList).filter(isValid);
