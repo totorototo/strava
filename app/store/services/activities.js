22a4971ea606b57266a8b64ecd46440c1d628b9c
toto rototo <el.totorototo@gmail.com>
2017-07-05 17:50:04 +0200

    update activity services.

--- app/store/services/activities.js
+++ app/store/services/activities.js
@@ -1,17 +1,58 @@
+import { schema, normalize } from "normalizr";
+
+import { pick } from "lodash";
+
 import { API_ENDPOINT, RESOURCES, METHODS } from "../constants/rest";
 
 import { callJSONApi } from "./helpers/api";
 
 export const getAthleteActivities = token => {
+  let d = new Date();
+  d.setMonth(d.getMonth() - 1);
+  const query = Math.round(d.getTime() / 1000);
+
   const request = {
     endpoint: {
       url: API_ENDPOINT + RESOURCES.ACTIVITIES,
       httpVerb: METHODS.GET
     },
-    token
+    token,
+    queryParameters: {
+      after: query
+    }
   };
   return callJSONApi(request).then(
-    response => ({ response: response.data }),
+    response => {
+      const activitySchema = new schema.Entity(
+        "activities",
+        {},
+        {
+          idAttribute: "id",
+          processStrategy: entity =>
+            pick(entity, [
+              "distance",
+              "total_elevation_gain",
+              "elapsed_time",
+              "achievement_count",
+              "pr_count",
+              "kudos_count",
+              "average_speed",
+              "max_speed",
+              "average_cadence",
+              "average_heartrate",
+              "max_heartrate",
+              "start_date"
+            ])
+        }
+      );
+      const activitiesSchema = [activitySchema];
+      const normalizedData = normalize(response.data, activitiesSchema);
+
+      return {
+        IDs: normalizedData.result,
+        entities: normalizedData.entities
+      };
+    },
     error => ({ error })
   );
 };
