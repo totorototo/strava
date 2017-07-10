e6387948bb19829153e0932f5719cb860d0c0999
toto rototo <el.totorototo@gmail.com>
2017-07-10 19:35:13 +0200

    clean up styles

--- app/routes/screens/performanceMeter/styles.js
+++ app/routes/screens/performanceMeter/styles.js
@@ -8,14 +8,5 @@ export default StyleSheet.create({
   },
   text: {
     color: "#FC4C02"
-  },
-  chart_title: {
-    paddingTop: 15,
-    textAlign: "center",
-    paddingBottom: 5,
-    paddingLeft: 5,
-    fontSize: 18,
-    color: "grey",
-    fontWeight: "bold"
   }
 });
