1a1c4e1152061f655e192a413db9d54cf2c89a91
toto rototo <el.totorototo@gmail.com>
2017-07-10 19:33:32 +0200

    update activities service.

--- app/store/services/activities.js
+++ app/store/services/activities.js
@@ -6,6 +6,8 @@ import { API_ENDPOINT, RESOURCES, METHODS } from "../constants/rest";
 
 import { callJSONApi } from "./helpers/api";
 
+import { references, referencesWeightings } from "../constants/references";
+
 export const getAthleteActivities = token => {
   const d = new Date();
   d.setMonth(d.getMonth() - 1);
@@ -61,32 +63,73 @@ export const computePerformance = (activities = {}) => {
   let distance = 0;
   let elevation = 0;
   let time = 0;
-  let records = 0;
-  let achievements = 0;
   let avgSpeed = 0;
 
   Object.keys(activities).forEach(id => {
     distance += activities[id].distance;
     elevation += activities[id].total_elevation_gain;
     time += activities[id].elapsed_time;
-    records += activities[id].pr_count;
-    achievements += activities[id].achievement_count;
     avgSpeed += activities[id].average_speed;
   });
 
   avgSpeed /= Object.keys(activities).length;
 
-  const performance = {
-    avgSpeed,
-    distance,
-    elevation,
-    popularity: 0,
-    time,
-    achievements,
-    stakhanov: Object.keys(activities).length,
-    records
-  };
+  const distanceHeuristic =
+    distance /
+    references.RECENT_RUN_DISTANCE *
+    referencesWeightings.RECENT_RUN_DISTANCE;
+
+  const elevationHeuristic =
+    elevation /
+    references.RECENT_RUN_ELEVATION_GAIN *
+    referencesWeightings.RECENT_RUN_ELEVATION_GAIN;
+
+  const frequencyHeuristic =
+    Object.keys(activities).length /
+    references.RECENT_RUN_COUNT *
+    referencesWeightings.RECENT_RUN_COUNT;
+
+  const speedHeuristic =
+    avgSpeed /
+    references.RECENT_RUN_SPEED *
+    referencesWeightings.RECENT_RUN_SPEED;
+
+  const timeHeuristic =
+    time / references.RECENT_RUN_TIME * referencesWeightings.RECENT_RUN_TIME;
+
+  const performance =
+    distanceHeuristic +
+    elevationHeuristic +
+    frequencyHeuristic +
+    speedHeuristic +
+    timeHeuristic;
+
+  const performanceDetails = [];
+  performanceDetails.push({
+    name: "distance",
+    number: Math.trunc(distanceHeuristic * 100 / performance)
+  });
+  performanceDetails.push({
+    name: "elevation",
+    number: Math.trunc(elevationHeuristic * 100 / performance)
+  });
+  performanceDetails.push({
+    name: "time",
+    number: Math.trunc(timeHeuristic * 100 / performance)
+  });
+  performanceDetails.push({
+    name: "speed",
+    number: Math.trunc(speedHeuristic * 100 / performance)
+  });
+  performanceDetails.push({
+    name: "frequency",
+    number: Math.trunc(frequencyHeuristic * 100 / performance)
+  });
+
   return {
-    performance
+    performance: {
+      details: performanceDetails,
+      value: performance
+    }
   };
 };
