a70339dac99667cafbda7a7a9b962797fbc90696
Toto RoToto <el.totorototo@gmail.com>
2017-07-19 23:11:43 +0200

    update race tile view

--- app/routes/screens/racePredictor/RacePredictor.js
+++ app/routes/screens/racePredictor/RacePredictor.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
 
-import { View } from "react-native";
+import { View, Dimensions, Text } from "react-native";
 import MapView from "react-native-maps";
 
+import { coordinates } from "./data";
 import styles from "./styles";
 
-class RacePredictor extends Component {
-  static finish() {
-    console.log("Countdown finished");
-  }
+const { width, height } = Dimensions.get("window");
 
+const ASPECT_RATIO = width / height;
+const LATITUDE = 42.78386;
+const LONGITUDE = 0.15844;
+const LATITUDE_DELTA = 0.37;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+
+const SAMPLE_REGION = {
+  latitude: LATITUDE,
+  longitude: LONGITUDE,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA
+};
+
+class RacePredictor extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <MapView
-          style={styles.map}
-          initialRegion={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421
-          }}
-        />
+        <MapView style={styles.map} initialRegion={SAMPLE_REGION}>
+          <MapView.Polyline
+            coordinates={coordinates}
+            strokeColor="#FC4C02"
+            fillColor="#FC4C02"
+            strokeWidth={3}
+          />
+        </MapView>
+
+        <View style={styles.buttonContainer}>
+          <View style={styles.bubble}>
+            <Text>Custom Tiles</Text>
+          </View>
+        </View>
       </View>
     );
   }
--- app/routes/screens/racePredictor/styles.js
+++ app/routes/screens/racePredictor/styles.js
@@ -2,11 +2,41 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
   container: {
-    ...StyleSheet.absoluteFillObject,
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
     justifyContent: "flex-end",
     alignItems: "center"
   },
   map: {
-    ...StyleSheet.absoluteFillObject
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  },
+  bubble: {
+    flex: 1,
+    backgroundColor: "rgba(255,255,255,0.7)",
+    paddingHorizontal: 18,
+    paddingVertical: 12,
+    borderRadius: 20
+  },
+  latlng: {
+    width: 200,
+    alignItems: "stretch"
+  },
+  button: {
+    width: 80,
+    paddingHorizontal: 12,
+    alignItems: "center",
+    marginHorizontal: 10
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    marginVertical: 20,
+    backgroundColor: "transparent"
   }
 });
