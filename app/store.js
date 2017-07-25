37f407cdac4233890177e4a66f8c4ae97b6be811
guipasmoi <guipasmoi@hotmail.com>
2017-07-25 18:34:02 +0200

    feat: reloaddux

--- app/store.js
+++ app/store.js
@@ -1,8 +1,36 @@
-import Store from "./store/Store";
-import rootReducer from "./store/state/rootReducer";
+import { Store } from "reloaddux";
 import sagaMonitor from "./devTools/sagaMonitor";
-import startingSaga from "./store/sagas/appSagas/index";
+import { authenticationFlowSaga } from "./store/sagas/appSagas/login";
+import { activitiesSaga } from "./store/sagas/activities";
+import { clubsSaga } from "./store/sagas/clubs";
 
-const store = new Store({ rootReducer, sagaMonitor, startingSaga });
+import rootReducer from "./store/state/rootReducer";
+import deeplink from "./store/deeplink";
+
+const store = new Store({
+  preloadedState: undefined,
+  sagaMonitor
+});
+
+deeplink(store);
+
+store.registerBusiness({
+  reducersTree: {
+    reducer: rootReducer,
+    default: rootReducer({}, { type: "@@redux/INIT" })
+  },
+  sagasMap: {
+    authenticationFlow: authenticationFlowSaga(),
+    athletactivitiesSagae: activitiesSaga(),
+    clubsSaga: clubsSaga()
+  }
+});
+
+store.dispatch({
+  type: "SET_ACCESS_TOKEN",
+  accessToken: "898741e906a19063f8e002336dbe9f3c04eb546b"
+});
+
+console.log(store.getState());
 
 export default store;
