8b08142c490ec5cf03fb3e8760798b3ab1842b52
guipasmoi <guipasmoi@hotmail.com>
2017-07-25 21:33:55 +0200

    feat: reloaddux implementation started
    * feat: reloaddux
    
    * fix: upgrade reloaddux dependency + remove console statement.

--- app/store.js
+++ app/store.js
@@ -1,8 +1,29 @@
-import Store from "./store/Store";
-import rootReducer from "./store/state/rootReducer";
+import { Store } from "reloaddux";
 import sagaMonitor from "./devTools/sagaMonitor";
-import startingSaga from "./store/sagas/appSagas/index";
+import { authenticationFlowSaga } from "./store/sagas/appSagas/login";
+import { activitiesSaga } from "./store/sagas/activities";
+import { clubsSaga } from "./store/sagas/clubs";
 
-const store = new Store({ rootReducer, sagaMonitor, startingSaga });
+import rootReducer from "./store/state/rootReducer";
+import deeplink from "./store/deeplink";
+
+const store = new Store({
+  preloadedState: undefined,
+  sagaMonitor
+});
+
+deeplink(store);
+
+store.registerBusiness({
+  reducersTree: {
+    reducer: rootReducer,
+    default: rootReducer({}, { type: "@@redux/INIT" })
+  },
+  sagasMap: {
+    authenticationFlowSaga,
+    activitiesSaga,
+    clubsSaga
+  }
+});
 
 export default store;
