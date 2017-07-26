233348caf2dd8b7bffd88cc201c979298eb68ba6
toto rototo <el.totorototo@gmail.com>
2017-07-26 17:08:28 +0200

    minor fixes due to strava api modifications. (breaking changes).

--- app/store/sagas/clubs.js
+++ app/store/sagas/clubs.js
@@ -8,44 +8,41 @@ import {
 import { updateEntity, setEntity } from "../actions/entities";
 import { setCurrentClubID } from "../actions/data";
 
-import { token } from "../state/appState/selectors";
+import { token, getCurrentUserID } from "../state/appState/selectors";
 
-import {
-  listClubMembers,
-  listClubAnnouncements,
-  listClubActivities
-} from "../services/clubs";
+import { listClubMembers, listClubActivities } from "../services/clubs";
 
 import { getStats } from "./athlete";
 import { getRankings } from "../services/activities";
 
 function* listMembers() {
   const accessToken = yield select(token);
+  const currentUserID = yield select(getCurrentUserID);
   const clubID = 288750;
   yield put(setCurrentClubID("loading"));
   const { ids, error } = yield call(listClubMembers, accessToken, clubID);
   if (!error) {
     yield put(setCurrentClubID(clubID));
     yield put(updateEntity(clubID, "clubs", { members: ids }));
-    for (let index = 0; index < ids.length; index += 1) {
-      yield getStats(ids[index]);
+    const filteredIds = ids.filter(id => id !== currentUserID);
+    for (let index = 0; index < filteredIds.length; index += 1) {
+      yield getStats(filteredIds[index]);
     }
   }
 }
 
-function* listAnnouncements() {
-  const accessToken = yield select(token);
-  const clubID = 288750;
-  const { ids, entities, error } = yield call(
-    listClubAnnouncements,
-    accessToken,
-    clubID
-  );
-  if (!error && ids && entities) {
-    yield put(updateEntity(clubID, "clubs", { ids }));
-    // yield put(setSubEntities(clubID, "clubs", entities));
-  }
-}
+// function* listAnnouncements() {
+//   const accessToken = yield select(token);
+//   const clubID = 288750;
+//   const { ids, entities, error } = yield call(
+//     listClubAnnouncements,
+//     accessToken,
+//     clubID
+//   );
+//   if (!error && ids && entities) {
+//     yield put(updateEntity(clubID, "clubs", { ids }));
+//   }
+// }
 
 function* listActivities() {
   const accessToken = yield select(token);
@@ -65,6 +62,7 @@ function* listActivities() {
 
 export function* clubsSaga() {
   yield takeEvery(SET_CURRENT_USER_ID, listMembers);
-  yield takeEvery(SET_CURRENT_CLUB_ID, listAnnouncements);
+  // TODO: strava api buggy? always returns void!
+  // yield takeEvery(SET_CURRENT_CLUB_ID, listAnnouncements);
   yield takeEvery(SET_CURRENT_CLUB_ID, listActivities);
 }
