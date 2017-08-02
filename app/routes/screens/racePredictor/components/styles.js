d53ea359707c5f0c0e030d839ab3a5470bcd5fe9
toto rototo <el.totorototo@gmail.com>
2017-08-02 17:50:41 +0200

    remove grid, col, row dependencies + code clean up

--- app/routes/screens/racePredictor/components/styles.js
+++ app/routes/screens/racePredictor/components/styles.js
@@ -9,19 +9,5 @@ export default StyleSheet.create({
   text: {
     color: "#FC4C02",
     fontSize: 15
-  },
-  grid: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    alignSelf: "center",
-    padding: 10
-  },
-  rowTop: {
-    justifyContent: "center",
-    alignItems: "flex-end"
-  },
-  rowLow: {
-    justifyContent: "center"
   }
 });
