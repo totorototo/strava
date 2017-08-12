3b003caae701eb8e46a5ed0619b6278c4dfa72ca
guipasmoi <guipasmoi@hotmail.com>
2017-08-12 09:27:54 +0200

    fix: AppNavigator refacto

--- app/App.js
+++ app/App.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 
 import { Provider } from "react-redux";
 
-import AppWithNavigationState from "./routes/AppWithNavigationState";
+import AppNavigator from "./routes/AppNavigator";
 
 import store from "./store";
 
@@ -11,7 +11,7 @@ export default class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppWithNavigationState />
+        <AppNavigator />
       </Provider>
     );
   }
