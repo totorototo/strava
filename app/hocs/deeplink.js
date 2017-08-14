3475d90200a2283551c7ef065a399efadb9ac30d
guipasmoi <guipasmoi@hotmail.com>
2017-08-14 22:12:32 +0200

    fix: lint may be

--- app/hocs/deeplink.js
+++ app/hocs/deeplink.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Linking /* Platform*/ } from "react-native";
+import { Linking } from "react-native";
 import hoistStatics from "hoist-non-react-statics";
 import Config from "react-native-config";
 
