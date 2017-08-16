1bde57c982375ea003561ebe17e6387be790fcd1
toto rototo <el.totorototo@gmail.com>
2017-08-16 14:59:58 +0200

    minor fix (typo).

---
+++ app/routes/screens/race/mapDispatchToProps.js
@@ -0,0 +1,8 @@
+import { bindActionCreators } from "redux";
+import * as geoloactionActions from "../../../store/actions/geolocation";
+
+const mapDispatchToProps = dispatch => ({
+  boundActionCreators: bindActionCreators(geoloactionActions, dispatch)
+});
+
+export default mapDispatchToProps;
