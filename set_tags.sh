23a8a5a3b6f5ea9c7f141fa53a58e07d1a40a310
ADeveloperHasNoName <el.totorototo@gmail.com>
2017-08-30 17:10:48 +0200

    Update set_tags.sh

--- set_tags.sh
+++ set_tags.sh
@@ -2,10 +2,10 @@ BRANCH="master"
 
 # Are we on the right branch?
 if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
-
+  
   # Is this not a Pull Request?
   if [ "$TRAVIS_PULL_REQUEST" = false ]; then
-
+    
     # Is this not a build which was triggered by setting a new tag?
     if [ -z "$TRAVIS_TAG" ]; then
       echo -e "Starting to tag commit.\n"
@@ -15,8 +15,8 @@ if [ "$TRAVIS_BRANCH" = "$BRANCH" ]; then
 
       # Add tag and push to master.
       git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
-      git push ci/travis --tags
-      git fetch ci/travis
+      git push origin --tags
+      git fetch origin
 
       echo -e "Done magic with tags.\n"
   fi
