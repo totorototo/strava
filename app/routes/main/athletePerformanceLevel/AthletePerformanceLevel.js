b7410fb75c713ecacf5352cf56dda09432b466e0
Toto RoToto <el.totorototo@gmail.com>
2017-10-07 12:34:29 +0200

    move logic to selector

--- app/routes/main/athletePerformanceLevel/AthletePerformanceLevel.js
+++ app/routes/main/athletePerformanceLevel/AthletePerformanceLevel.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { compose } from "redux";
 import { View } from "react-native";
+import { compose } from "redux";
 
 import enhanceWithValidEntities from "../../../hocs/enhanceWithValidEntities";
 import selector from "./selector";
@@ -11,30 +11,29 @@ import Progress from "../../../components/common/progress/Progress";
 
 class AthletePerformanceLevel extends Component {
   static propTypes = {
-    athlete: PropTypes.shape({
-      firstname: PropTypes.string,
-      lastname: PropTypes.string,
-      profil: PropTypes.string,
-      performance: PropTypes.any
+    performance: PropTypes.shape({
+      details: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string,
+          percent: PropTypes.number,
+          values: PropTypes.number,
+          unit: PropTypes.number
+        }).isRequired
+      ),
+      value: PropTypes.number.isRequired
     }).isRequired
   };
 
   render() {
     return (
       <View style={styles.container}>
-        <Progress
-          progress={
-            this.props.athlete.performance
-              ? this.props.athlete.performance.value
-              : NaN
-          }
-        />
+        <Progress progress={this.props.performance.value} />
       </View>
     );
   }
 }
 
 export default compose(
-  enhanceWithValidEntities(({ athlete }) => [athlete]),
+  enhanceWithValidEntities(({ performance }) => [performance]),
   connect(selector)
 )(AthletePerformanceLevel);
--- app/routes/main/athletePerformanceLevel/selector.js
+++ app/routes/main/athletePerformanceLevel/selector.js
@@ -1,10 +1,19 @@
 import { getCurrentUserID } from "../../../store/state/appState/selectors";
-import { getEntity } from "../../../store/state/entities/selectors";
+import {
+  getEntity,
+  getValueFrom
+} from "../../../store/state/entities/selectors";
+// import { isValid } from "../../../dataDefinitions/defects";
 
 const athletePerformanceLevelViewSelector = state => {
   const currentUserID = getCurrentUserID(state);
+  const performance = getValueFrom(
+    getEntity(state, "athletes", currentUserID),
+    "performance"
+  );
+
   return {
-    athlete: getEntity(state, "athletes", currentUserID)
+    performance
   };
 };
 
