044936b206780e30b195d327a3e1aa5fa1fa6249
Toto RoToto <el.totorototo@gmail.com>
2017-10-10 23:25:28 +0200

    merge entry point ios / android

--- index.android.js
+++
@@ -1,6 +0,0 @@
-import "es6-symbol/implement";
-import { AppRegistry } from "react-native";
-
-import App from "./app/App";
-
-AppRegistry.registerComponent("lybitos", () => App);
--- ios/lybitos/AppDelegate.m
+++ ios/lybitos/AppDelegate.m
@@ -26,15 +26,15 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   NSURL *jsCodeLocation;
-  
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
-  
+
+  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"lybitos"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-  
+
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
