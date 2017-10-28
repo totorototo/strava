47dcd554ffbbaa54bc2e826bd007d7495add060b
Toto RoToto <el.totorototo@gmail.com>
2017-10-28 19:45:39 +0200

    WIP: add decoded maps to selector.

--- app/routes/main/clubFeed/selector.js
+++ app/routes/main/clubFeed/selector.js
@@ -5,8 +5,68 @@ import {
   getValidEntities
 } from "../../../store/state/entities/selectors";
 
+const decodePolyline = (encoded = "") => {
+  if (!encoded) {
+    return [];
+  }
+  const poly = [];
+  let index = 0;
+  const len = encoded.length;
+  let lat = 0;
+  let lng = 0;
+
+  while (index < len) {
+    let b;
+    let shift = 0;
+    let result = 0;
+
+    do {
+      b = encoded.charCodeAt((index += 1)) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+
+    const dlat = (result & 1) !== 0 ? ~(result >> 1) : result >> 1;
+    lat += dlat;
+
+    shift = 0;
+    result = 0;
+
+    do {
+      b = encoded.charCodeAt((index += 1)) - 63;
+      result |= (b & 0x1f) << shift;
+      shift += 5;
+    } while (b >= 0x20);
+
+    const dlng = (result & 1) !== 0 ? ~(result >> 1) : result >> 1;
+    lng += dlng;
+
+    const p = {
+      latitude: lat / 1e5,
+      longitude: lng / 1e5
+    };
+    poly.push(p);
+  }
+
+  return poly;
+};
+
 const clubFeedViewSelector = state => {
   const currentClub = getEntity(state, "clubs", getCurrentClubID(state));
+
+  const activities = getValidEntities(
+    state,
+    "activities",
+    getValueFrom(currentClub, "activities")
+  );
+
+  const mapsIDs = activities.map(activity => activity.map);
+  const maps = getValidEntities(state, "maps", mapsIDs);
+  const modifiedMaps = maps.map(map => ({
+    id: map.id,
+    coordinates: decodePolyline(map.summary_polyline)
+  }));
+
   return {
     club: currentClub,
     clubMembers: getValidEntities(
@@ -14,11 +74,8 @@ const clubFeedViewSelector = state => {
       "athletes",
       getValueFrom(currentClub, "members")
     ),
-    activities: getValidEntities(
-      state,
-      "activities",
-      getValueFrom(currentClub, "activities")
-    )
+    activities,
+    maps: modifiedMaps
   };
 };
 
