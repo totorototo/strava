b7b06eb93ea79c16aead5c32efbbffa8c0f11a64
toto rototo <el.totorototo@gmail.com>
2018-01-03 13:46:46 +0100

    code clean up

--- app/store/services/helpers/gps.js
+++ app/store/services/helpers/gps.js
@@ -1,12 +1,12 @@
 const computeDistance = (
-  pointA = { longitude: 0, latitude: 0 },
-  pointB = { longitude: 0, latitude: 0 }
+  fromLocation = { longitude: 0, latitude: 0 },
+  toLocation = { longitude: 0, latitude: 0 }
 ) => {
   const R = 6371e3; // metres
-  const φ1 = pointA.latitude * Math.PI / 180;
-  const φ2 = pointB.latitude * Math.PI / 180;
-  const Δφ = (pointB.latitude - pointA.latitude) * Math.PI / 180;
-  const Δλ = (pointB.longitude - pointA.longitude) * Math.PI / 180;
+  const φ1 = fromLocation.latitude * Math.PI / 180;
+  const φ2 = toLocation.latitude * Math.PI / 180;
+  const Δφ = (toLocation.latitude - fromLocation.latitude) * Math.PI / 180;
+  const Δλ = (toLocation.longitude - fromLocation.longitude) * Math.PI / 180;
 
   const a =
     Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
@@ -16,42 +16,42 @@ const computeDistance = (
   return R * c / 1000;
 };
 
-const isInArea = (
-  position = { longitude: 0, latitude: 0 },
-  point = { longitude: 0, latitude: 0 },
+const isInRegion = (
+  currentLocation = { longitude: 0, latitude: 0 },
+  reference = { longitude: 0, latitude: 0 },
   Δλ = 0,
   Δφ = 0
 ) =>
-  Math.abs(position.longitude - point.longitude) < Δλ &&
-  Math.abs(position.latitude - point.latitude) < Δφ;
+  Math.abs(currentLocation.longitude - reference.longitude) < Δλ &&
+  Math.abs(currentLocation.latitude - reference.latitude) < Δφ;
 
-const computePathDistance = (points = []) =>
-  points.reduce((distance, currentPoint, index) => {
-    const nextPoint = points[index + 1];
+const computePathDistance = (locations = []) =>
+  locations.reduce((distance, currentPoint, index) => {
+    const nextPoint = locations[index + 1];
     if (nextPoint) {
       return distance + computeDistance(currentPoint, nextPoint);
     }
     return distance;
   }, 0);
 
-const findClosestPoint = (points = [], currentPosition = {}) => {
-  const distances = points.map((currentPoint, index) => ({
+const findClosestPoint = (locations = [], currentLocation = {}) => {
+  const distances = locations.map((currentPoint, index) => ({
     index,
-    distance: computeDistance(currentPosition, currentPoint)
+    distance: computeDistance(currentLocation, currentPoint)
   }));
 
   const sortedDistances = distances.sort((a, b) => a.distance - b.distance);
   if (sortedDistances.length > 0) {
-    return points[sortedDistances[0].index];
+    return locations[sortedDistances[0].index];
   }
-  return currentPosition;
+  return currentLocation;
 };
 
-const computeElevationGain = (points = []) =>
-  points.reduce((elevationGain, currentPoint, index) => {
-    const previousPoint = points[index - 1];
+const computeElevationGain = (locations = []) =>
+  locations.reduce((elevationGain, currentLocation, index) => {
+    const previousPoint = locations[index - 1];
     if (previousPoint) {
-      const Δφ = currentPoint.altitude - previousPoint.altitude;
+      const Δφ = currentLocation.altitude - previousPoint.altitude;
       if (Δφ > 0) {
         return elevationGain + Δφ;
       }
@@ -60,9 +60,38 @@ const computeElevationGain = (points = []) =>
     return elevationGain;
   }, 0);
 
+const computeRegion = (locations = []) =>
+  locations.reduce(
+    (region, currentLocation) => ({
+      minLongitude:
+        currentLocation.longitude < region.minLongitude
+          ? currentLocation.longitude
+          : region.minLongitude,
+      maxLongitude:
+        currentLocation.longitude > region.maxLongitude
+          ? currentLocation.longitude
+          : region.maxLongitude,
+      minLatitude:
+        currentLocation.latitude < region.minLatitude
+          ? currentLocation.latitude
+          : region.minLatitude,
+      maxLatitude:
+        currentLocation.latitude > region.maxLongitude
+          ? currentLocation.latitude
+          : region.maxLongitude
+    }),
+    {
+      minLongitude: locations[0].longitude,
+      maxLongitude: locations[0].longitude,
+      minLatitude: locations[0].latitude,
+      maxLatitude: locations[0].latitude
+    }
+  );
+
 export default {
+  computeRegion,
   computeDistance,
-  isInArea,
+  isInRegion,
   computePathDistance,
   computeElevationGain,
   findClosestPoint
