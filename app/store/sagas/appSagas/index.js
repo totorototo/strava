595182d164fa7afb7b9b8ae30a6b103fc4a2c3bc
Toto RoToto <el.totorototo@gmail.com>
2018-02-14 21:24:58 +0100

    Merge branch 'feat/elevationProfile' of https://github.com/totorototo/strava into feat/elevationProfile
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/store/sagas/appSagas/index.js
+++ app/store/sagas/appSagas/index.js
@@ -1,15 +1,11 @@
 import { authenticationFlowSaga } from "./login";
 import { activitiesSaga } from "../activities";
 import { clubsSaga } from "../clubs";
-import { remoteDataSaga } from "../remoteData/data";
 import { raceSaga } from "../race";
-import { locationSaga } from "../geolocation";
 
 export default {
   authenticationFlowSaga,
   activitiesSaga,
   clubsSaga,
-  remoteDataSaga,
-  raceSaga,
-  locationSaga
+  raceSaga
 };
