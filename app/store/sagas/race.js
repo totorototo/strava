926e0ebb2a81aeeb37eaaf2c30e6c9e9cefbd1ed
Toto RoToto <el.totorototo@gmail.com>
2018-02-14 21:22:15 +0100

    update race saga. now dealing with edges and nodes

--- app/store/sagas/race.js
+++ app/store/sagas/race.js
@@ -4,16 +4,32 @@ import { SET_CURRENT_USER_ID } from "../constants/actionTypes";
 import { setCurrentRaceID } from "../actions/data";
 import { setEntity } from "../actions/entities";
 import { markers, coordinates } from "./data/data";
+import positionHelper from "../services/helpers/gps";
 
 function* listRaces() {
-  // 1- mock strava api call
+  const edges = coordinates.reduce((accu, location, index) => {
+    if (coordinates[index + 1]) {
+      const length = positionHelper.computeDistanceBetweenLocations(
+        location,
+        coordinates[index + 1]
+      );
+      const edge = {
+        src: location,
+        dest: coordinates[index + 1],
+        length
+      };
+      return [...accu, edge];
+    }
+    return accu;
+  }, []);
+
   const entities = {};
   const raceID = 123456;
   entities[raceID] = {
     date: "2018-08-24T03:00:00+00:00",
     checkPoints: markers,
     path: {
-      coordinates
+      edges
     }
   };
   yield put(setCurrentRaceID(raceID));
