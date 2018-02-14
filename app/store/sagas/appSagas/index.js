3d2dbc88de8e241146327f5c8e18ee722f316597
Toto RoToto <el.totorototo@gmail.com>
2018-02-15 00:00:48 +0100

    make location sharing great again

--- app/store/sagas/appSagas/index.js
+++ app/store/sagas/appSagas/index.js
@@ -2,10 +2,12 @@ import { authenticationFlowSaga } from "./login";
 import { activitiesSaga } from "../activities";
 import { clubsSaga } from "../clubs";
 import { raceSaga } from "../race";
+import { locationSaga } from "../geolocation";
 
 export default {
   authenticationFlowSaga,
   activitiesSaga,
   clubsSaga,
-  raceSaga
+  raceSaga,
+  locationSaga
 };
