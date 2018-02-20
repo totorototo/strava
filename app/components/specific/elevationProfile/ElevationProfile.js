5477967dc619f7337e5d25ac9dce8c2cb062f399
Toto RoToto <el.totorototo@gmail.com>
2018-02-20 21:24:20 +0100

    WIP - split profile into section.

--- app/components/specific/elevationProfile/ElevationProfile.js
+++ app/components/specific/elevationProfile/ElevationProfile.js
@@ -61,8 +61,75 @@ export default class ElevationProfile extends Component {
   }
 
   static createAreaGraph(edges, graphWidth, graphHeight) {
-    // TODO: create area foreach partition
-    // const partitions = positionHelper.partitionPath(...edges);
+    // temp
+    let distanceDone = 0;
+
+    // helper
+    const convertPercentToGrade = percent => {
+      let grade;
+      if (Math.abs(percent) < 5) {
+        grade = "ELEVATION_GRADE.SMALL";
+      } else if (Math.abs(percent) >= 5 && Math.abs(percent) < 7) {
+        grade = "ELEVATION_GRADE.MEDIUM";
+      } else if (Math.abs(percent) >= 7 && Math.abs(percent) < 10) {
+        grade = "ELEVATION_GRADE.LARGE";
+      } else if (Math.abs(percent) >= 10 && Math.abs(percent) < 15) {
+        grade = "ELEVATION_GRADE.HUGE";
+      } else if (Math.abs(percent) >= 15) {
+        grade = "ELEVATION_GRADE.OHMYGOD";
+      }
+      return grade;
+    };
+
+    const convertGradeToColor = grade => {
+      if (grade === "ELEVATION_GRADE.SMALL") {
+        return "#f4f6f5";
+      } else if (grade === "ELEVATION_GRADE.MEDIUM") {
+        return "#ECBC3E";
+      } else if (grade === "ELEVATION_GRADE.LARGE") {
+        return "#EA8827";
+      } else if (grade === "ELEVATION_GRADE.HUGE") {
+        return "#E1351D";
+      } else if (grade === "ELEVATION_GRADE.OHMYGOD") {
+        return "#96451F";
+      }
+      return "#daaddd";
+    };
+
+    // add view model data
+    const updatedEdges = edges.reduce((accu, edge, index) => {
+      const length = positionHelper.computeDistance(edge);
+      distanceDone += length;
+      const elevation = (edge.dest.altitude - edge.src.altitude) / 1000;
+      const percent = elevation / length * 100;
+      const grade = convertPercentToGrade(percent);
+      const color = convertGradeToColor(grade);
+
+      const enhancedEdge = {
+        edge,
+        length,
+        percent,
+        index,
+        distanceDone,
+        grade,
+        color
+      };
+      return [...accu, enhancedEdge];
+    }, []);
+
+    // grouping by
+    let currentGrade = "";
+    const sortedEdges = updatedEdges.reduce((accu, edge) => {
+      if (currentGrade === edge.grade) {
+        return [
+          ...accu.slice(0, accu.length - 1),
+          ...accu.slice(accu.length),
+          [...accu[accu.length - 1], edge]
+        ];
+      }
+      currentGrade = edge.grade;
+      return [...accu, [edge]];
+    }, []);
 
     // Create our x-scale.
     const scaleX = ElevationProfile.createXScale(
@@ -83,39 +150,43 @@ export default class ElevationProfile extends Component {
     // Create our y-scale.
     const scaleY = ElevationProfile.createYScale(0, extentY[1], graphHeight);
 
-    // Use the d3-shape line generator to create the `d={}` attribute value.
-    const areaShape = d3.shape
-      .area()
-      // For every x and y-point in our line shape we are given an item from our
-      // array which we pass through our scale function so we map the domain value
-      // to the range value.
-      .x(d => scaleX(ElevationProfile.computeDistance(edges, d)))
-      .y1(d => scaleY(d.src.altitude))
-      .y0(extentY[1])
-      .curve(d3.shape.curveNatural);
+    const areas = sortedEdges.map(section => {
+      const areaShape = d3.shape
+        .area()
+        // For every x and y-point in our line shape we are given an item from our
+        // array which we pass through our scale function so we map the domain value
+        // to the range value.
+        .x(d => scaleX(d.distanceDone))
+        .y1(d => scaleY(d.edge.src.altitude))
+        .y0(extentY[1])
+        .curve(d3.shape.curveNatural);
 
-    return {
-      // Pass in our array of data to our line generator to produce the `d={}`
-      // attribute value that will go into our `<Shape />` component.
-      path: areaShape(edges)
-    };
+      return {
+        path: areaShape(section),
+        color: section[0].color
+      };
+    });
+
+    return areas;
   }
 
   render() {
     const { path } = this.props;
     const { width } = Dimensions.get("window");
 
-    const lineGraph = ElevationProfile.createAreaGraph(path.edges, width, 100);
+    const areas = ElevationProfile.createAreaGraph(path.edges, width, 100);
 
     return (
       <View style={styles.container}>
         <Surface width={width} height={100}>
-          <Shape
-            d={lineGraph.path}
-            stroke="#fdb799"
-            fill="#fdb799"
-            strokeWidth={1}
-          />
+          {areas.map(area => (
+            <Shape
+              d={area.path}
+              stroke={area.color}
+              fill={area.color}
+              strokeWidth={1}
+            />
+          ))}
         </Surface>
       </View>
     );
