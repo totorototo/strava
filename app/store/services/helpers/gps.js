42d7a8801708e1ec92bdef6ff1dd3cfedaf2b527
toto rototo <el.totorototo@gmail.com>
2018-02-21 16:38:00 +0100

    add region computation

--- app/store/services/helpers/gps.js
+++ app/store/services/helpers/gps.js
@@ -16,6 +16,64 @@ const computeDistanceBetweenLocations = (
   return R * c / 1000;
 };
 
+const computeRegionForLocations = (locations = []) =>
+  locations.reduce(
+    (region, currentLocation) => ({
+      minLongitude:
+        currentLocation.longitude < region.minLongitude
+          ? currentLocation.longitude
+          : region.minLongitude,
+      maxLongitude:
+        currentLocation.longitude > region.maxLongitude
+          ? currentLocation.longitude
+          : region.maxLongitude,
+      minLatitude:
+        currentLocation.latitude < region.minLatitude
+          ? currentLocation.latitude
+          : region.minLatitude,
+      maxLatitude:
+        currentLocation.latitude > region.maxLongitude
+          ? currentLocation.latitude
+          : region.maxLongitude
+    }),
+    {
+      minLongitude: locations[0].longitude,
+      maxLongitude: locations[0].longitude,
+      minLatitude: locations[0].latitude,
+      maxLatitude: locations[0].latitude
+    }
+  );
+
+const computeRegion = (...edges) => {
+  const initialRegion = {
+    minLatitude: edges[0].src.latitude || 0,
+    maxLatitude: edges[0].src.latitude || 0,
+    minLongitude: edges[0].src.longitude || 0,
+    maxLongitude: edges[0].src.longitude || 0
+  };
+  return edges.reduce((region, edge) => {
+    const currentRegion = computeRegionForLocations([edge.src, edge.dest]);
+    return {
+      minLatitude:
+        currentRegion.minLatitude < region.minLatitude
+          ? currentRegion.minLatitude
+          : region.minLatitude,
+      maxLatitude:
+        currentRegion.maxLatitude > region.maxLatitude
+          ? currentRegion.maxLatitude
+          : region.maxLatitude,
+      minLongitude:
+        currentRegion.minLongitude < region.minLongitude
+          ? currentRegion.minLongitude
+          : region.minLongitude,
+      maxLongitude:
+        currentRegion.maxLongitude > region.maxLongitude
+          ? currentRegion.maxLongitude
+          : region.maxLongitude
+    };
+  }, initialRegion);
+};
+
 const computeDistance = (...edges) =>
   edges.reduce(
     (distance, edge) =>
@@ -56,5 +114,6 @@ export default {
   computeDistance,
   findClosestEdge,
   computeElevationGain,
-  computeDistanceBetweenLocations
+  computeDistanceBetweenLocations,
+  computeRegion
 };
