a7a82a8fc28a2806201cff07c5810d2a95251cd1
Toto RoToto <el.totorototo@gmail.com>
2018-02-24 19:11:34 +0100

    code clean-up

--- app/store/services/event.js
+++ app/store/services/event.js
@@ -1,7 +1,8 @@
 import { coordinates, markers } from "../sagas/data/data";
 import positionHelper from "./helpers/gps";
+import { convertPercentToGrade } from "./helpers/event";
 
-const getEdges = (...locations) => {
+const getEventEdges = (...locations) => {
   const result = locations.reduce((accu, location, index) => {
     if (coordinates[index + 1]) {
       const length = positionHelper.computeDistanceBetweenLocations(
@@ -20,18 +21,34 @@ const getEdges = (...locations) => {
   return result;
 };
 
-export const getEventDetails = (id = 0) => {
-  const edges = getEdges(...coordinates);
+const getEdgesElevationDetails = (...edges) => {
+  let distanceDone = 0;
+  return edges.reduce((accu, edge, index) => {
+    const length = positionHelper.computeDistance(edge);
+    distanceDone += length;
+    const elevation = (edge.dest.altitude - edge.src.altitude) / 1000;
+    const percent = elevation / length * 100;
+    const grade = convertPercentToGrade(percent);
 
+    return [...accu, { ...edge, percent, index, distanceDone, grade }];
+  }, []);
+};
+
+export const getEventDetails = (eventID = 0, withElevationDetails = true) => {
   const entities = {};
-  const raceID = id;
+
+  const edges = getEventEdges(...coordinates);
+  const raceID = eventID;
   entities[raceID] = {
     date: "2018-08-24T03:00:00+00:00",
     checkPoints: markers,
-    path: {
-      edges
-    }
+    path: { edges }
   };
+
+  if (withElevationDetails) {
+    const detailedEdges = getEdgesElevationDetails(...edges);
+    entities[raceID].path.edges = detailedEdges;
+  }
 
   return entities;
 };
