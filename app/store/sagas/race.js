38fc2ef56d176cb1ca49317723f33120f69c0a86
Toto RoToto <el.totorototo@gmail.com>
2018-02-24 13:11:53 +0100

    add service for race/event

--- app/store/sagas/race.js
+++ app/store/sagas/race.js
@@ -1,37 +1,14 @@
-import { put, takeEvery } from "redux-saga/effects";
+import { put, call, takeEvery } from "redux-saga/effects";
 
 import { SET_CURRENT_USER_ID } from "../constants/actionTypes";
 import { setCurrentRaceID } from "../actions/data";
 import { setEntity } from "../actions/entities";
-import { markers, coordinates } from "./data/data";
-import positionHelper from "../services/helpers/gps";
+import { getEventDetails } from "../services/event";
 
 function* listRaces() {
-  const edges = coordinates.reduce((accu, location, index) => {
-    if (coordinates[index + 1]) {
-      const length = positionHelper.computeDistanceBetweenLocations(
-        location,
-        coordinates[index + 1]
-      );
-      const edge = {
-        src: location,
-        dest: coordinates[index + 1],
-        length
-      };
-      return [...accu, edge];
-    }
-    return accu;
-  }, []);
-
-  const entities = {};
   const raceID = 123456;
-  entities[raceID] = {
-    date: "2018-08-24T03:00:00+00:00",
-    checkPoints: markers,
-    path: {
-      edges
-    }
-  };
+  const entities = yield call(getEventDetails, raceID);
+
   yield put(setCurrentRaceID(raceID));
   yield put(setEntity("races", entities));
 }
--- app/store/services/event.js
+++ app/store/services/event.js
@@ -1,49 +1,8 @@
 import { coordinates, markers } from "../sagas/data/data";
 import positionHelper from "./helpers/gps";
-import { convertGradeToColor, convertPercentToGrade } from "./helpers/event";
 
-const compose = (...fns) => x => fns.reduceRight((v, f) => f(v), x);
-
-const addData = (...edges) => {
-  let distanceDone = 0;
-  return edges.reduce((accu, edge, index) => {
-    const length = positionHelper.computeDistance(edge);
-    distanceDone += length;
-    const elevation = (edge.dest.altitude - edge.src.altitude) / 1000;
-    const percent = elevation / length * 100;
-    const grade = convertPercentToGrade(percent);
-    const color = convertGradeToColor(grade);
-
-    const enhancedEdge = {
-      edge,
-      length,
-      percent,
-      index,
-      distanceDone,
-      grade,
-      color
-    };
-    return [...accu, enhancedEdge];
-  }, []);
-};
-
-const groupBy = (...edges) => {
-  let currentGrade = "";
-  return edges.reduce((accu, edge) => {
-    if (currentGrade === edge.grade) {
-      return [
-        ...accu.slice(0, accu.length - 1),
-        ...accu.slice(accu.length),
-        [...accu[accu.length - 1], edge]
-      ];
-    }
-    currentGrade = edge.grade;
-    return [...accu, [edge]];
-  }, []);
-};
-
-const getEdges = (...locations) =>
-  locations.reduce((accu, location, index) => {
+const getEdges = (...locations) => {
+  const result = locations.reduce((accu, location, index) => {
     if (coordinates[index + 1]) {
       const length = positionHelper.computeDistanceBetweenLocations(
         location,
@@ -58,9 +17,12 @@ const getEdges = (...locations) =>
     }
     return accu;
   }, []);
+  return result;
+};
 
 export const getEventDetails = (id = 0) => {
-  const edges = compose(groupBy, addData, getEdges(...coordinates))(id);
+  const edges = getEdges(...coordinates);
+
   const entities = {};
   const raceID = id;
   entities[raceID] = {
