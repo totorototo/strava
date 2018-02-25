36c72ca6ddc236cf787d54b82b9133abe72806a6
Toto RoToto <el.totorototo@gmail.com>
2018-02-25 19:57:06 +0100

    code clean-up

--- app/components/specific/elevationProfile/ElevationProfile.js
+++ app/components/specific/elevationProfile/ElevationProfile.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { last, flatten } from "lodash";
 import PropTypes from "prop-types";
 import { View, ART, Dimensions } from "react-native";
 import * as scale from "d3-scale";
@@ -14,6 +15,44 @@ const d3 = {
   scale,
   shape,
   d3Array
+};
+
+const getRange = percent => {
+  if (Math.abs(percent) < 5) {
+    return ELEVATION_GRADE.SMALL;
+  } else if (Math.abs(percent) >= 5 && Math.abs(percent) < 7) {
+    return ELEVATION_GRADE.EASY;
+  } else if (Math.abs(percent) >= 7 && Math.abs(percent) < 10) {
+    return ELEVATION_GRADE.MEDIUM;
+  } else if (Math.abs(percent) >= 10 && Math.abs(percent) < 15) {
+    return ELEVATION_GRADE.DIFFICULT;
+  } else if (Math.abs(percent) >= 15) {
+    return ELEVATION_GRADE.HARD;
+  }
+  return ELEVATION_GRADE.UNKNOWN;
+};
+
+const groupEdgesByRange = function(accu, item) {
+  const val = getRange(item.percent);
+  // eslint-disable-next-line no-param-reassign
+  accu[val] = accu[val] || [];
+  accu[val].push(item);
+  return accu;
+};
+
+const groupEdgesBySequence = function(result, value, index, collection) {
+  if (index > 0 && value.index - collection[index - 1].index === 1) {
+    const group = last(result);
+    group.push(value);
+  } else {
+    result.push([value]);
+  }
+  return result;
+};
+
+// eslint-disable-next-line no-extend-native
+Array.prototype.groupBy = function(fn, accu) {
+  return this.reduce(fn, accu);
 };
 
 export default class ElevationProfile extends Component {
@@ -54,48 +93,20 @@ export default class ElevationProfile extends Component {
     );
   }
 
-  static createColorScale() {
+  static createColorScale2() {
     return d3.scale
       .scaleThreshold()
-      .domain([5, 7, 10, 15])
+      .domain([1, 2, 3, 4])
       .range(["#f2f0f7", "#ECBC3E", "#EA8827", "#E1351D", "#96451F"]);
   }
 
-  // TODO: here?
-  static getElevationRange(percent) {
-    if (Math.abs(percent) < 5) {
-      return ELEVATION_GRADE.SMALL;
-    } else if (Math.abs(percent) >= 5 && Math.abs(percent) < 7) {
-      return ELEVATION_GRADE.EASY;
-    } else if (Math.abs(percent) >= 7 && Math.abs(percent) < 10) {
-      return ELEVATION_GRADE.MEDIUM;
-    } else if (Math.abs(percent) >= 10 && Math.abs(percent) < 15) {
-      return ELEVATION_GRADE.DIFFICULT;
-    } else if (Math.abs(percent) >= 15) {
-      return ELEVATION_GRADE.HARD;
-    }
-    return ELEVATION_GRADE.UNKNOWN;
-  }
-
-  // TODO: should be prototypeOn array and take a grouping fct that return the key of an item
-  static groupBy = (...edges) => {
-    let currentElevationRange = "";
-    return edges.reduce((accu, edge) => {
-      const elevationRange = ElevationProfile.getElevationRange(edge.percent);
-      if (currentElevationRange === elevationRange) {
-        return [
-          ...accu.slice(0, accu.length - 1),
-          ...accu.slice(accu.length),
-          [...accu[accu.length - 1], edge]
-        ];
-      }
-      currentElevationRange = elevationRange;
-      return [...accu, [edge]];
-    }, []);
-  };
-
   static createAreaGraph(edges, graphWidth, graphHeight) {
-    const groupedEdges = ElevationProfile.groupBy(...edges);
+    const groupedEdgesByRange = edges.groupBy(groupEdgesByRange, {});
+
+    const groupedEdgesBySequence = {};
+    Object.entries(groupedEdgesByRange).forEach(([range, section]) => {
+      groupedEdgesBySequence[range] = section.groupBy(groupEdgesBySequence, []);
+    });
 
     // Create our x-scale.
     const scaleX = ElevationProfile.createXScale(
@@ -104,7 +115,7 @@ export default class ElevationProfile extends Component {
       graphWidth
     );
 
-    const colorScale = ElevationProfile.createColorScale();
+    const colorScale = ElevationProfile.createColorScale2();
 
     // Collect all y values.
     const altitudes = edges.reduce(
@@ -118,22 +129,32 @@ export default class ElevationProfile extends Component {
     // Create our y-scale.
     const scaleY = ElevationProfile.createYScale(0, extentY[1], graphHeight);
 
-    return groupedEdges.map(section => {
-      const areaShape = d3.shape
-        .area()
-        // For every x and y-point in our line shape we are given an item from our
-        // array which we pass through our scale function so we map the domain value
-        // to the range value.
-        .x(d => scaleX(d.distanceDone))
-        .y1(d => scaleY(d.src.altitude))
-        .y0(extentY[1])
-        .curve(d3.shape.curveNatural);
+    const test = Object.entries(
+      groupedEdgesBySequence
+    ).map(([grade, sequences]) =>
+      sequences.map(sequence => {
+        const areaShape = d3.shape
+          .area()
+          // For every x and y-point in our line shape we are given an item from our
+          // array which we pass through our scale function so we map the domain value
+          // to the range value.
+          .x(d => scaleX(d.distanceDone))
+          .y1(d => scaleY(d.src.altitude))
+          .defined(d => typeof d.value !== "string")
+          .y0(extentY[1])
+          .curve(d3.shape.curveNatural);
 
-      return {
-        path: areaShape(section),
-        color: colorScale(Math.abs(section[0].percent))
-      };
-    });
+        return {
+          path: areaShape(sequence),
+          color: colorScale(grade)
+        };
+      })
+    );
+
+    const sections = Object.values(test).map(sequences =>
+      sequences.map(item => item)
+    );
+    return flatten(sections);
   }
 
   render() {
