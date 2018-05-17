4f847afc59c600ef27c9051358e30cee6415ed84
totorototo <el.totorototo@gmail.com>
2018-05-17 17:22:18 +0200

    link against fs package

--- android/app/src/main/java/com/lybitos/MainApplication.java
+++ android/app/src/main/java/com/lybitos/MainApplication.java
@@ -3,6 +3,7 @@ package com.lybitos;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactInstanceManager;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new MapsPackage(),
             new ReactNativeConfigPackage()
       );
